27b4817d4d1d38bccfc06a80d0b7c8afd808c603
keiliduo <1075004260@qq.com>
2018-08-21 14:32:13 +0800

    这是第一个控制台程序
    Signed-off-by: keiliduo <1075004260@qq.com>

---
+++ yingyong/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace yingyong
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine("this is first console application");
+        }
+    }
+}
